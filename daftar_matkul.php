dbdfe280b793021b609e78d9ecdd1ed1be048477
dhimasnugroho0705@gmail.com <desmitaaprilia@gmail.com>
2022-11-24 01:12:11 +0700

    First Commit Dhimas

---
+++ daftar_matkul.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Daftar Mata Kuliah SMT 5</title>
+		<style>
+			body{
+				padding-top: 75px;
+				background-color: powderblue;
+			}
+			.th{
+			font-size: 18px;
+			padding-top: 3px;
+			background-color: yellow;
+		}
+			.td{
+				text-align: center;
+			}
+			.tf{
+				font-weight: bold;
+				text-align: center;
+				font-size: 18px;
+			}
+			.table{}
+		</style>
+	</head>
+	<body>
+		<center>
+		<table border="1">
+			<thead>
+				<tr>
+					<th colspan="4" class="th">
+						<center>Kartu Rencana Studi</center>
+					</th>
+				</tr>
+				<tr>
+					<th>Nomor</th>
+					<th>Kode</th>
+					<th>Nama Mata Kuliah</th>
+					<th>SKS</th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr>
+					<td colspan="3" class="tf">
+						<center>Total SKS</center>
+					</td>
+					<td class="tf">
+						24
+					</td>
+				</tr>
+			</tfoot>
+			<tbody>
+				<tr>
+					<td class="td">1</td>
+					<td class="td">IKT013118</td>
+					<td>Interaksi Digital</td>
+					<td class="td">3</td>
+				</tr>
+				<tr>
+					<td class="td">2</td>
+					<td class="td">TIF253008</td>
+					<td>Interaksi Manusia Komputer</td>
+					<td class="td">3</td>
+				</tr>
+				<tr>
+					<td class="td">3</td>
+					<td class="td">TIF263118</td>
+					<td>Grafika Komputer</td>
+					<td class="td">3</td>
+				</tr>
+				<tr>
+					<td class="td">4</td>
+					<td class="td">TIF310318</td>
+					<td>Pemrograman Berbasis Web</td>
+					<td class="td">2</td>
+				</tr>
+				<tr>
+					<td class="td">5</td>
+					<td class="td">TIF310418</td>
+					<td>Praktikum Pemrograman Berbasis Web</td>
+					<td class="td">1</td>
+				</tr>
+				<tr>
+					<td class="td">6</td>
+					<td class="td">TIFWP22101</td>
+					<td>Kalkulus</td>
+					<td class="td">2</td>
+				</tr>
+				<tr>
+					<td class="td">7</td>
+					<td class="td">TIFWP22102</td>
+					<td>Matematika Diskrit</td>
+					<td class="td">2</td>
+				</tr>
+				<tr>
+					<td class="td">8</td>
+					<td class="td">TIFWP22103</td>
+					<td>Statistika dan Probabilitas</td>
+					<td class="td">2</td>
+				</tr>
+				<tr>
+					<td class="td">9</td>
+					<td class="td">UTR093018</td>
+					<td>Sistem Ekonomi Pancasila</td>
+					<td class="td">3</td>
+				</tr>
+				<tr>
+					<td class="td">10</td>
+					<td class="td">PUTR113118</td>
+					<td>Praktikum Keteknososiopreneuran</td>
+					<td class="td">3</td>
+				</tr>
+			</tbody>
+		</table>
+		</center>
+	</body>
+</html>
