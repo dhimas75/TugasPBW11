9ca4977b18a2d2abf4ea1290844c38ab0761ff1b
Dhimas N A <dhimasnugroho0705@gmail.com>
2022-11-23 22:47:58 +0700

    Posting pertama file dosen.php

---
+++ dosen.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Belajar ngoding</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<table border = "1">
+    <tr>
+        <th>No. </th>
+        <th>Mata Kuliah Semester 5</th>
+        <th>Nama Dosen Pengajar</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Interaksi Digital</td>
+        <td>Mhd. Idham Khalif,S.Kom.,M.T.</td>
+    </tr>
+     <tr>
+        <td>2</td>
+        <td>Statistika dan Probablitas</td>
+        <td>Ninuk Wiliani,S.Si,M.Kom</td>
+    </tr>
+     <tr>
+        <td>3</td>
+        <td>Matematika Distrik</td>
+        <td>Ninuk Wiliani,S.Si,M.Kom</td>
+    </tr>
+     <tr>
+        <td>4</td>
+        <td>Grafika Komputer</td>
+        <td>Silvester Dian Handy Permana,ST.,MTI,CDS,CPro</td>
+    </tr>
+     <tr>
+        <td>5</td>
+        <td>Kalkulus</td>
+        <td>Dewi Lestari,M.Si</td>
+    </tr>
+     <tr>
+        <td>6</td>
+        <td>Ptaktik keteknososiopreneuran</td>
+        <td>Yocki Pramudya A,SE.MM.</td>
+    </tr>
+     <tr>
+        <td>7</td>
+        <td>Pemrograman Berbasis Web</td>
+        <td>Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
+    </tr>
+     <tr>
+        <td>8</td>
+        <td>Praktik Pemrograman Berbasis Web</td>
+        <td>Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>Interaksi Manusia Komputer</td>
+        <td>Assoc.Prof.Budi Ariftiama,S.T.,M.M.S.I</td>
+    </tr>
+    <tr>
+        <td>10</td>
+        <td>Sistem Ekonomi Pancasila</td>
+        <td>Lestari Agus Salim</td>
+    </tr>
+
+</table>
+
+</body>
+</html>
