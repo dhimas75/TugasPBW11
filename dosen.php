e7e762ec99403ee1c91470a6e00864316544d6d6
Dhimas N A <dhimasnugroho0705@gmail.com>
2022-11-23 23:20:03 +0700

    Perubahan ke 1 file dosen.php

--- dosen.php
+++ dosen.php
@@ -2,65 +2,81 @@
 <html>
 <head>
 <title>Belajar ngoding</title>
-<link rel="stylesheet" href="style.css">
+
+<style>
+			body{
+				padding-top: 75px;
+                padding-left: 350px;
+				background-color: powderblue;
+			}
+			.th{
+			font-size: 18px;
+			padding-top: 3px;
+			background-color: yellow;
+		}
+			.td{
+				text-align: center;
+			}
+	</style>
+
 </head>
 <body>
-    
+
 <table border = "1">
     <tr>
-        <th>No. </th>
-        <th>Mata Kuliah Semester 5</th>
-        <th>Nama Dosen Pengajar</th>
+        <th class="th">No. </th>
+        <th class="th">Mata Kuliah Semester 5</th>
+        <th class="th">Nama Dosen Pengajar</th>
     </tr>
     <tr>
-        <td>1</td>
-        <td>Interaksi Digital</td>
-        <td>Mhd. Idham Khalif,S.Kom.,M.T.</td>
+        <td class="td">1</td>
+        <td class="td">Interaksi Digital</td>
+        <td class="td">Mhd. Idham Khalif,S.Kom.,M.T.</td>
     </tr>
      <tr>
-        <td>2</td>
-        <td>Statistika dan Probablitas</td>
-        <td>Ninuk Wiliani,S.Si,M.Kom</td>
+        <td class="td">2</td>
+        <td class="td">Statistika dan Probablitas</td>
+        <td class="td">Ninuk Wiliani,S.Si,M.Kom</td>
     </tr>
      <tr>
-        <td>3</td>
-        <td>Matematika Distrik</td>
-        <td>Ninuk Wiliani,S.Si,M.Kom</td>
+        <td class="td">3</td>
+        <td class="td">Matematika Distrik</td>
+        <td class="td">Ninuk Wiliani,S.Si,M.Kom</td>
     </tr>
      <tr>
-        <td>4</td>
-        <td>Grafika Komputer</td>
-        <td>Silvester Dian Handy Permana,ST.,MTI,CDS,CPro</td>
+        <td class="td">4</td>
+        <td class="td">Grafika Komputer</td>
+        <td class="td">Silvester Dian Handy Permana,ST.,MTI,CDS,CPro</td>
     </tr>
      <tr>
-        <td>5</td>
-        <td>Kalkulus</td>
-        <td>Dewi Lestari,M.Si</td>
+        <td class="td">5</td>
+        <td class="td">Kalkulus</td>
+        <td class="td">Dewi Lestari,M.Si</td>
     </tr>
      <tr>
-        <td>6</td>
-        <td>Ptaktik keteknososiopreneuran</td>
-        <td>Yocki Pramudya A,SE.MM.</td>
+        <td class="td">6</td>
+        <td class="td">Ptaktik keteknososiopreneuran</td>
+        <td class="td">Yocki Pramudya A,SE.MM.</td>
     </tr>
      <tr>
-        <td>7</td>
-        <td>Pemrograman Berbasis Web</td>
-        <td>Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
+        <td class="td">7</td>
+        <td class="td">Pemrograman Berbasis Web</td>
+        <td class="td">Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
     </tr>
      <tr>
-        <td>8</td>
-        <td>Praktik Pemrograman Berbasis Web</td>
-        <td>Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
+        <td class="td">8</td>
+        <td class="td">Praktik Pemrograman Berbasis Web</td>
+        <td class="td">Ir. Yaddarabullah,M.Kom,Asean Eng.</td>
     </tr>
     <tr>
-        <td>9</td>
-        <td>Interaksi Manusia Komputer</td>
-        <td>Assoc.Prof.Budi Ariftiama,S.T.,M.M.S.I</td>
+        <td class="td">9</td>
+        <td class="td">Interaksi Manusia Komputer</td>
+        <td class="td">Assoc.Prof.Budi Ariftiama,S.T.,M.M.S.I</td>
     </tr>
     <tr>
-        <td>10</td>
-        <td>Sistem Ekonomi Pancasila</td>
-        <td>Lestari Agus Salim</td>
+        <td class="td">10</td>
+        <td class="td">Sistem Ekonomi Pancasila</td>
+        <td class="td">Lestari Agus Salim</td>
     </tr>
 
 </table>
